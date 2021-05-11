561583a4d7eb62d4726830cb19168b18ed371c20
Marlon Ferreira da Costa <marlon.costa@flexpag.local>
2021-05-11 00:16:44 -0300

    First commit

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+    selector:'app-course-list',
+    templateUrl:'./course-list.component.html'
+})
+export class CourseListComponent implements OnInit{
+
+    courses: Course[]=[];
+
+
+    ngOnInit(): void{
+
+        this.courses = [{
+            id: 1,
+            name: 'Angular Form',
+            imageUrl: 'assets/images/forms.png',
+            price: 99.99,
+            code: 'XPS-123',
+            duration: 120,
+            rating: 5.4,
+            releaseDate:'December, 2, 2019'
+        },
+        {
+            id: 2,
+            name: 'Angular HTTP',
+            imageUrl: 'assets/images/http.png',
+            price: 45.99,
+            code: 'XPS-3445',
+            duration: 80,
+            rating: 4,
+            releaseDate:'December, 4, 2019'
+        }
+    ]
+    }
+}
